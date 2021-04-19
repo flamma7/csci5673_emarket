b494f2627926b59d22cd7e9c76039a05f8cc47af
Luke Barbier <lukebarbier12@gmail.com>
2021-04-19 10:11:41 -0600

    update ips

--- install.bash
+++ install.bash
@@ -1,7 +1,8 @@
 sudo apt install python3-pip
-python3 -m pip install grpcio grpcio_tools flask requests spyne lxml suds-py3 pysyncobj
+python3 -m pip install --upgrade pip
+python3 -m pip install grpcio grpcio_tools flask requests spyne lxml suds-py3 pysyncobj python-dotenv
 python setup.py install
 cd emarket
-python -m grpc_tools.protoc -I../protos --python_out=. --grpc_python_out=. ../protos/product.proto
-python -m grpc_tools.protoc -I../protos --python_out=. --grpc_python_out=. ../protos/customer.proto
+python3 -m grpc_tools.protoc -I../protos --python_out=. --grpc_python_out=. ../protos/product.proto
+python3 -m grpc_tools.protoc -I../protos --python_out=. --grpc_python_out=. ../protos/customer.proto
 cd ..
